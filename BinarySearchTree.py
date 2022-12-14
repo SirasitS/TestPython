08dc718b446dc7b63ca5571159ee7385836b6a03
Sirasit Sudnakrob <sirasit.sudnakrob@seagate.com>
2022-12-14 09:12:55 +0700

    Extraction condition in contains function.

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -3,11 +3,10 @@ import collections
 Node = collections.namedtuple('Node', ['left', 'right', 'value'])
 
 def contains(root, value):
-    if root is None or root.value == value:
-        if root is None:            
-            return False
+    if root is None:
+        return False
+    if root.value == value:
         return True
-
     if root.value < value:        
         return contains(root.right, value)    
     return contains(root.left, value)
