64c8d98f954e703f45366563ce4e4953e76e7ddb
Sirasit Sudnakrob <sirasit.sudnakrob@seagate.com>
2022-12-14 13:16:07 +0700

    UserInput.py usecase

--- UserInput.py
+++ UserInput.py
@@ -1,8 +1,20 @@
 class TextInput:
-    pass
+    def __init__(self):
+        self.textResult = ""
+
+    def add(self, character):
+        self.textResult += character
+
+    def get_value(self):
+        return self.textResult
   
-class NumericInput:
-    pass
+
+class NumericInput(TextInput):
+    def add(self, character):
+        if character.isnumeric():
+            self.textResult += character
+        return self.textResult        
+
 
 if __name__ == '__main__':
     input = NumericInput()
