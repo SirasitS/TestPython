9dc1d43498a8ec9bea4e3f26f35cb6783fecd95f
Sirasit Sudnakrob <sirasit.sudnakrob@seagate.com>
2022-12-16 16:41:16 +0700

    Leage Table commit

--- LeagueTable.py
+++ LeagueTable.py
@@ -12,32 +12,46 @@ class LeagueTable:
     def player_rank(self, rank):
         if rank > len(self.standings):
             return None
-        # print("length of self.standings =", len(self.standings))
-        print("Current table scores =", self.standings)
-
-        score_list = sorted({v['score'] for k,v in self.standings.items()}, reverse=True)
-        print("scores =", score_list)
-        print("score of rank {0} = {1}".format(rank, score_list[rank-1]))
-
-        min_play =  min({v2['games_played'] for k2,v2 in self.standings.items()})
-        print("min game play =", min_play)
-
-        highest_player_score = {k:v['games_played'] for k,v in self.standings.items() if v['score'] == score_list[rank-1]}
-        print("The top score player =", highest_player_score)
-        soretd_game = sorted(highest_player_score, key=lambda x: highest_player_score[x], reverse=False)        # Still bug
-        print("sort game play =", soretd_game)
         
-        print("First player =", soretd_game[0])
+        print("Current table scores =", self.standings)        
+        # Sort table by score
+        table_list = sorted([(k,v['score'],v['games_played']) for k,v in self.standings.items()], key=lambda s:s[1], reverse=True)
+        print("table_list = {0} and table_count = {1}".format(table_list, len(table_list)))
 
-        # if len(highest_player_score) >= 2:
-        #     pass
-        # return highest_player_score[0]
+        target_score = table_list[rank-1][1]        
+        # max_score =  max({v['score'] for k,v in self.standings.items()})
+        target_count = len([(k,v['score'],v['games_played']) for k,v in self.standings.items() if v['score'] == target_score])
+        print("target_score = {0} and target_count = {1}".format(target_score, target_count))
+
+        if target_count > 1:
+            target_player = [t for t in table_list if t[1] == target_score]
+            print("target_playert =", target_player)    
+            game_sorted = sorted(target_player, key=lambda s:s[2], reverse=False)
+            print("game_sorted = {0} and game_sorted_count = {1}".format(game_sorted, len(game_sorted)))   
+            print("Player rank {0} = {1}".format(rank, game_sorted[0][0]))   
+            return game_sorted[0][0]
+
+        # print("player_list = {0} and player_count = {1}".format(player_list, len(player_list)))
+
+        # min_play =  min({v2['games_played'] for k2,v2 in self.standings.items()})
+        # print("min game play =", min_play)
+
+        # highest_player_score = {k:v['games_played'] for k,v in self.standings.items() if v['score'] == score_list[rank-1]}
+        # print("highest_player_score =", highest_player_score)
+        
+        # soretd_player = sorted(highest_player_score, key=lambda x: highest_player_score[x], reverse=False)
+        # print("sorted player =", soretd_player)
+        print("Player rank {0} = {1}".format(rank, table_list[rank-1][0]))
+        return table_list[rank-1][0]
 
 if __name__ == "__main__":
     table = LeagueTable(['Mike', 'Chris', 'Arnold'])
     table.record_result('Mike', 2)
-    table.record_result('Mike', 1)
+    table.record_result('Mike', 7)
+    # table.record_result('Mike', 5)
     table.record_result('Arnold', 5)
-    table.record_result('Chris', 2)
-    table.record_result('Chris', 3)
-    print(table.player_rank(1))
+    table.record_result('Chris', 5)
+    # table.record_result('Chris', 2)
+    print("Player Rank 1 = {0}\nPlayer Rank 2 = {1}\nPlayer Rank 3 = {2}\n".format(table.player_rank(1), table.player_rank(2), table.player_rank(3)))
+    # print("Player Rank 2 =", table.player_rank(2))
+    # print("Player Rank 3 =", table.player_rank(3))
