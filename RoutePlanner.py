aa214b9a878a61d10f230b3f11758f69b9d530bb
Sirasit Sudnakrob <sirasit.sudnakrob@seagate.com>
2022-12-19 16:53:27 +0700

    Commit before go back home

--- RoutePlanner.py
+++ RoutePlanner.py
@@ -18,13 +18,15 @@ def route_exists(from_row, from_column, to_row, to_column, map_matrix):
     # print("temp_route =", temp_route)
 
     while list_path:
+        print("Before pop list_path =", list_path)
         current_row, current_column = list_path.pop(0)        
         print(current_row, current_column)
+        print("After pop list_path =", list_path)
 
         if current_row == to_row and current_column == to_column:            
             return True
 
-        end_neighbore = True
+        # end_neighbore = True
 
         for row_offset, col_offset in zip(row_offsets, col_offsets):
             # compute the row and column indices of the neighboring cell
@@ -34,20 +36,18 @@ def route_exists(from_row, from_column, to_row, to_column, map_matrix):
             # check if the neighboring cell is within the bounds of the map and is a road
             if (0 <= neighbor_row < row_count and 0 <= neighbor_col < col_count and map_matrix[neighbor_row][neighbor_col]):
                 # mark the neighboring cell as visited
-                # map_matrix[neighbor_row][neighbor_col] = False
-
-                # temp_route[from_row][from_column] = True
-                end_neighbore = False
+                map_matrix[neighbor_row][neighbor_col] = False
+                print("map_matrix =", map_matrix)
+                # end_neighbore = False
 
                 # add the neighboring cell to the queue
                 list_path.append((neighbor_row, neighbor_col))
-
-        # if all the neightboring cells were False and not equal target (to_row, to_column), Set current cell to False
-        if end_neighbore :
-            map_matrix[neighbor_row][neighbor_col] = False
         
-
-
+        # if all the neightboring cells were False and not equal target (to_row, to_column), Set current cell to False
+        # if end_neighbore:
+        #     map_matrix[current_row][current_column] = False      
+        #     print(map_matrix)      
+        
     return False
 
     # for i, row in enumerate(map_matrix):     
